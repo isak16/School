c7f58da11a39d263f1fa89fb16838df007c1b6bc
Isak Larsson <Isak454@hotmail.com>
2017-02-27 17:59:32 +0100

    New folders

---
+++ Lektions misc/170227/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by isak16 on 2017-02-20.
+ */
+var app = angular.module('app', ['ngMessages']);
+
+app.controller('TestController', function($scope) {
+
+    $scope.signIn = function(){
+        alert("daamn daniel");
+    }
+
+});
+
+
